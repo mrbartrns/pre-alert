9e9057a5cc58f1ac826b4f84dc8c87284d3c6517
Junhyuck Ko <mrbartrns@naver.com>
2022-09-29 10:30:59 +0900

    initial commit

---
+++ README.md
@@ -0,0 +1,25 @@
+# pre-alert
+
+> 데스크톱 환경에서 사용할 수 있는 미리 알림 웹입니다.
+
+## install
+
+```bash
+yarn install
+```
+
+## start
+
+```bash
+yarn start
+```
+
+## build
+```bash
+yarn build
+```
+
+
+# contributing
+
+# license
---
+++ webpack.config.js
@@ -0,0 +1,67 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+const ForkTsCheckerWebpackPlugin = require('fork-ts-checker-webpack-plugin');
+
+module.exports = {
+  resolve: {
+    extensions: ['.js', '.jsx', '.ts', '.tsx'],
+    alias: {
+      '@': path.resolve(__dirname, 'src'),
+      '@components': path.resolve(__dirname, 'src/components'),
+      '@hooks': path.resolve(__dirname, 'src/hooks'),
+    },
+  },
+  entry: './src/index.tsx',
+  output: {
+    filename: '[name].[chunkhash].js',
+    path: path.resolve(__dirname, 'dist'),
+    publicPath: '/',
+    clean: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(t|j)sx?$/,
+        use: 'swc-loader',
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.s?css$/,
+        use: ['style-loader', 'css-loader', 'postcss-loader', 'sass-loader'],
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, 'src/index.html'),
+    }),
+    new CopyWebpackPlugin({
+      patterns: [
+        {
+          from: 'public',
+        },
+      ],
+    }),
+    new ForkTsCheckerWebpackPlugin({
+      typescript: {
+        diagnosticOptions: {
+          semantic: true,
+          syntactic: true,
+        },
+      },
+    }),
+  ],
+  devServer: {
+    port: 3000,
+    hot: true,
+    host: 'localhost',
+    historyApiFallback: true,
+  },
+  optimization: {
+    splitChunks: {
+      chunks: 'all',
+    },
+  },
+};
